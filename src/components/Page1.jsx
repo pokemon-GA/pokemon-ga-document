8b553ba7f619d428f937ab29fc9d60b9da26ea6b
wakkun-0516 <yuwaku0516@icloud.com>
2024-02-24 01:49:52 +0900

    EV・QUのリンク先を付けて置きました。

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 
 const Page1div = styled.div`
     background-color: #000;
+    padding-bottom: 600px;
+
+    #ev {
+        padding-top: 70px;
+    }
 `;
 
 const FlexDiv = styled.div`
@@ -37,6 +42,7 @@ const Page1 = () => {
                     text={'対戦した際のポケモンの評価を計算する関数の作成'}
                     button={'notHidden'}
                     buttonText={'About evaluation function'}
+                    link={'/#ev'}
                 />
                 <PageCard
                     title={'Web Application'}
@@ -55,18 +61,28 @@ const Page1 = () => {
                     text={'妥当な環境の制限を探求する。'}
                     button={'notHidden'}
                     buttonText={'About Quest'}
+                    link={'/#qu'}
                 />
             </CardDiv>
-            <PageText
-                title={'Evaluation Function'}
-                lang={''}
-                langClass={'hidden'}
-                text={
-                    'ポケモン１対１を、技選択の探索によって評価。その評価をベースに、３体選出ルールを考慮した評価値を算出。'
-                }
-            />
-            <PageTextDiv>
-                <PageText title={'Quest'} lang={''} langClass={'hidden'} text={'svシーズン1の使用率top100のポケモンのみで構成された環境になっている。ポケモンの型は、ポケモン対戦にある程度知見がある2人が、育成論の記事を読んでメジャーであると感じたものを独断と偏見で採用。'} />
+            <div id="ev">
+                <PageText
+                    title={'Evaluation Function'}
+                    lang={''}
+                    langClass={'hidden'}
+                    text={
+                        'ポケモン１対１を、技選択の探索によって評価。その評価をベースに、３体選出ルールを考慮した評価値を算出。'
+                    }
+                />
+            </div>
+            <PageTextDiv id="qu">
+                <PageText
+                    title={'Quest'}
+                    lang={''}
+                    langClass={'hidden'}
+                    text={
+                        'svシーズン1の使用率top100のポケモンのみで構成された環境になっている。ポケモンの型は、ポケモン対戦にある程度知見がある2人が、育成論の記事を読んでメジャーであると感じたものを独断と偏見で採用。'
+                    }
+                />
             </PageTextDiv>
         </Page1div>
     );
